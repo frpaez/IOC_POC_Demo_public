d77e0b93191cdd2ea671fa32f0309aac2e15cc69
Cesar Roman <cromansantillan@deloitte.com>
2022-08-11 16:17:28 -0500

    Adding Singin-TC_SIGNIN_005 and Object Repository-Page_MySmartStore

---
+++ Object Repository/Page_MySmartStore Ecommerce/Page_Shop. Register/p_Your registration completed.rs
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_Your registration completed</name>
+   <tag></tag>
+   <elementGuidId>9640b568-00dd-4f25-9091-66b9fe993870</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='content-center']/div/div[2]/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>p.section-body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>2b75bb1e-8e2a-4850-8280-ed0df892f2fa</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>section-body</value>
+      <webElementGuid>09c4c080-8e20-4e2e-8a8f-e1fd9ad2031f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            Your registration completed
+        </value>
+      <webElementGuid>a7fcfeb2-b82f-46bd-bea9-5f52e6a7ff66</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;content-center&quot;)/div[@class=&quot;page registration-result-page&quot;]/div[@class=&quot;page-body&quot;]/p[@class=&quot;section-body&quot;]</value>
+      <webElementGuid>315b2644-6a3d-4426-b97b-fbe5704c3157</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='content-center']/div/div[2]/p</value>
+      <webElementGuid>741c9999-efed-497d-8377-226022d97ad4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Register'])[1]/following::p[1]</value>
+      <webElementGuid>e3bf7c83-2048-4590-b987-0cf85df3dc5d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sunglasses'])[1]/following::p[1]</value>
+      <webElementGuid>0c1c426c-1306-4825-ad5d-49e4db7b2cf3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Continue'])[1]/preceding::p[1]</value>
+      <webElementGuid>b4ebcebd-ba4b-4c5d-a3b9-8337d216e736</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Informations'])[1]/preceding::p[1]</value>
+      <webElementGuid>4fc41e52-3fb4-485e-b781-40f734015435</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Your registration completed']/parent::*</value>
+      <webElementGuid>188f016d-0d8d-4829-a5c9-de59eb65a0d6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//p</value>
+      <webElementGuid>be0ba532-23ae-48de-bed9-2d471318ebe1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = '
+            Your registration completed
+        ' or . = '
+            Your registration completed
+        ')]</value>
+      <webElementGuid>a0ed4fb6-2de0-425e-bd56-7bd4f586b26c</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_MySmartStore Ecommerce/Page_Shop/a_croman1.rs
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_croman1</name>
+   <tag></tag>
+   <elementGuidId>df42efe0-f351-41f2-8462-df6d8690cab7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//nav[@id='menubar-my-account']/div/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#menubar-my-account > div.dropdown > a.menubar-link</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>b6259f58-c27b-4290-b5ce-2dd945ed5854</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>menubar-link</value>
+      <webElementGuid>72766243-8d04-4a38-81e5-8b49234f8654</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-toggle</name>
+      <type>Main</type>
+      <value>dropdown</value>
+      <webElementGuid>c0737054-522d-4eec-9572-6d26201dab96</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-haspopup</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>9b3c284c-597f-4735-91d8-eebba15bfb49</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-expanded</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>35de4cc0-487f-4b3c-be6c-a588f5f0e749</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/samples/TestComplete12/smartstore/Customer/Info</value>
+      <webElementGuid>5b690638-e26e-42a4-8491-2064149a46b5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>rel</name>
+      <type>Main</type>
+      <value>nofollow</value>
+      <webElementGuid>e347940e-fec0-48c5-9a7d-20549b38658f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                    
+
+					    croman1
+                        
+                </value>
+      <webElementGuid>3eef99f5-bf63-4ec9-b1db-9e03762bfb4a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;menubar-my-account&quot;)/div[@class=&quot;dropdown&quot;]/a[@class=&quot;menubar-link&quot;]</value>
+      <webElementGuid>ba37e7f6-9418-4196-9826-29b5c524de09</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//nav[@id='menubar-my-account']/div/a</value>
+      <webElementGuid>f45d7029-7ced-4e87-bbb3-6b9235605869</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Forums'])[1]/following::a[1]</value>
+      <webElementGuid>cb8cdb24-0478-4b7d-9d00-0faa44823569</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Blog'])[1]/following::a[2]</value>
+      <webElementGuid>fc218103-112a-47c1-bf09-9466dbb73522</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='My account'])[1]/preceding::a[1]</value>
+      <webElementGuid>553a5524-c177-4610-8104-e27d3722b8d0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/samples/TestComplete12/smartstore/Customer/Info')]</value>
+      <webElementGuid>ed7a15a2-fb24-44fb-9052-c40fa712c642</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//nav[4]/div/a</value>
+      <webElementGuid>0c4f07db-6f86-40bd-abf1-5cc85b77819e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/samples/TestComplete12/smartstore/Customer/Info' and (text() = '
+                    
+
+					    croman1
+                        
+                ' or . = '
+                    
+
+					    croman1
+                        
+                ')]</value>
+      <webElementGuid>c107b8d8-3232-4c4c-8e0c-37202739de37</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
